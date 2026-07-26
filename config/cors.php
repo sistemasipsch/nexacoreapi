7d36eaab82d9ce2d234b19347f304c15c105bdde
Junior Arias <junior.arias02yt@gmail.com>
2026-07-26 10:18:07 -0500

    feat: acceso publico api

--- config/cors.php
+++ config/cors.php
@@ -13,7 +13,9 @@ return [
 
     'allowed_origins' => array_map('trim', explode(',', env('CORS_ALLOWED_ORIGINS', ''))),
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => env('CORS_ALLOWED_ORIGINS_PATTERNS')
+        ? array_map('trim', explode(',', env('CORS_ALLOWED_ORIGINS_PATTERNS')))
+        : (in_array('*', array_map('trim', explode(',', env('CORS_ALLOWED_ORIGINS', '')))) ? ['.*'] : []),
 
     'allowed_headers' => ['*'],
 
